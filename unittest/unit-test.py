ebff1b3cdc0fcee2cd46bdb1969acb26c7ec9e6a
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-04-05 17:21:01 +0200

    dataloader is passed unittest

---
+++ unittest/unit-test.py
@@ -0,0 +1,41 @@
+import sys
+import os
+import unittest
+import torch
+
+sys.path.append("src/")
+
+from config import RAW_DATA_PATH, PROCESSED_DATA_PATH
+from utils import dump, load
+
+
+class UnitTest(unittest.TestCase):
+
+    def setUp(self):
+        if os.path.exists(PROCESSED_DATA_PATH):
+            self.dataloader = load(
+                filename=os.path.join(PROCESSED_DATA_PATH, "dataloader.pkl")
+            )
+            self.train_dataloader = load(
+                filename=os.path.join(PROCESSED_DATA_PATH, "train_dataloader.pkl")
+            )
+            self.test_dataloader = load(
+                filename=os.path.join(PROCESSED_DATA_PATH, "test_dataloader.pkl")
+            )
+
+        else:
+            raise Exception("Data not found".capitalize())
+
+        self.total_data = 100
+
+    def tearDown(self):
+        self.total_data = None
+
+    def test_total_datasets(self):
+        self.assertEquals(
+            sum(noise.size(0) for noise, clean in self.dataloader), self.total_data
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
