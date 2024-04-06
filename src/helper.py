d8571cbe4ac2932a0187faca11f6e1525c217e23
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-04-06 19:24:42 +0200

    doc strings is added in helpers method

--- src/helper.py
+++ src/helper.py
@@ -13,6 +13,49 @@ from DnCNN import DnCNN
 
 
 def helper(**kwargs):
+    """
+    Initializes and configures the DnCNN model, optimizer, loss criterion, and data loaders.
+
+    This function sets up the DnCNN model with specified training parameters, selects the optimizer based on user
+    preferences (Adam or SGD), chooses the loss function (Huber Loss or MSE Loss), and loads the training, testing,
+    and complete datasets. It also applies weight initialization to the model and moves the model to the specified
+    computation device.
+
+    Parameters:
+        device (str): The device to train the model on ('cuda', 'mps', or 'cpu').
+        adam (bool): If True, use Adam optimizer; otherwise, check for SGD.
+        SGD (bool): If True and adam is False, use SGD optimizer.
+        lr (float): Learning rate for the optimizer.
+        beta1 (float): Beta1 hyperparameter for the Adam optimizer.
+        huber_loss (bool): If True, use Huber loss; otherwise, use MSE loss.
+        lr_scheduler (bool): If True, use learning rate scheduler.
+
+    Returns:
+        dict: A dictionary containing the initialized model, optimizer, loss criterion, data loaders, and scheduler.
+
+    Raises:
+        FileNotFoundError: If the processed data directory does not exist or dataloaders could not be found.
+
+    Note:
+        - The function relies on the `params` function to load model-specific parameters like beta1 for Adam, step_size,
+          and gamma for the StepLR scheduler, and momentum for SGD from a YAML file.
+        - It applies a predefined weight initialization scheme to the DnCNN model before training.
+        - All components are configured and returned in a dictionary for easy access.
+
+    Example:
+        >>> config = {
+                "device": "cuda",
+                "adam": True,
+                "SGD": False,
+                "lr": 1e-3,
+                "beta1": 0.9,
+                "huber_loss": False,
+                "lr_scheduler": True
+            }
+        >>> setup = helper(**config)
+        >>> print(setup["model"])
+        >>> print(setup["train_dataloader"].dataset)
+    """
     model = DnCNN()
     device = kwargs["device"]
     adam = kwargs["adam"]
