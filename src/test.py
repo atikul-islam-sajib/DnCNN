69ae3b0a69ce799f978c5e1fa78dd3db646ad3a1
Atikul Islam Sajib <112280249+atikul-islam-sajib@users.noreply.github.com>
2024-04-06 14:20:38 +0200

    doc strings is added in test

--- src/test.py
+++ src/test.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import argparse
 import matplotlib.pyplot as plt
 import imageio
 import torch
@@ -18,7 +19,43 @@ from DnCNN import DnCNN
 
 
 class Charts:
+    """
+    A class used to represent and perform operations for model visualization, including
+    selecting the best model based on loss, loading data, normalizing images, creating GIFs from
+    training images, and plotting comparison images (noisy, clean, predicted) using Matplotlib.
+
+    Attributes:
+        device (str): The device type (e.g., 'cpu', 'cuda', 'mps') for PyTorch operations.
+        infinity (float): A high initial value for comparing and finding minimum loss.
+        model (torch.nn.Module): The neural network model used for image denoising.
+        best_model (torch.nn.Module): The best performing model based on loss.
+        details (dict): A dictionary containing details of the best model and its loss.
+
+    Methods:
+        select_best_model(self):
+            Selects the best model based on minimum loss from saved models.
+
+        dataloader(self):
+            Loads the dataloader from the processed data path.
+
+        image_normalized(self, image):
+            Normalizes an image tensor to be in the range [0, 1].
+
+        create_gif(self):
+            Creates a GIF from training images stored in a specified path.
+
+        plot(self):
+            Plots comparison images (noisy, clean, predicted) and saves a comparison figure and a training images GIF.
+    """
+
     def __init__(self, device="mps"):
+        """
+        Initializes the Charts object with specified device for PyTorch operations and
+        sets initial values for attributes.
+
+        Parameters:
+            device (str): The device type for PyTorch operations. Defaults to 'mps'.
+        """
         self.device = device_init(device=device)
         self.infinity = float("inf")
         self.model = None
@@ -26,6 +63,15 @@ class Charts:
         self.details = {"best_model": list(), "best_loss": list()}
 
     def select_best_model(self):
+        """
+        Selects the best model based on minimum loss from models saved in the BEST_MODELS_PATH.
+
+        Returns:
+            torch.nn.Module: The best performing model based on loss.
+
+        Raises:
+            Exception: If the BEST_MODELS_PATH does not exist.
+        """
         if os.path.exists(BEST_MODELS_PATH):
 
             models = os.listdir(BEST_MODELS_PATH)
@@ -48,16 +94,40 @@ class Charts:
             raise Exception("Best model path is not found".title())
 
     def dataloader(self):
+        """
+        Loads and returns the dataloader object from a pickled file in the PROCESSED_DATA_PATH.
+
+        Returns:
+            DataLoader: The dataloader containing the dataset.
+
+        Raises:
+            Exception: If the PROCESSED_DATA_PATH does not exist or the dataloader file is not found.
+        """
         if os.path.exists(PROCESSED_DATA_PATH):
             return load(os.path.join(PROCESSED_DATA_PATH, "dataloader.pkl"))
         else:
             raise Exception("Processed data path is not found".title())
 
     def image_normalized(self, image):
+        """
+        Normalizes an image tensor to have values in the range [0, 1].
+
+        Parameters:
+            image (torch.Tensor): The image tensor to be normalized.
+
+        Returns:
+            numpy.ndarray: The normalized image as a numpy array.
+        """
         image = image.cpu().detach().numpy()
         return (image - image.min()) / (image.max() - image.min())
 
     def create_gif(self):
+        """
+        Creates and saves a GIF from images stored in TRAIN_IMAGES_PATH to GIF_IMAGE_PATH.
+
+        Raises:
+            Exception: If the TRAIN_IMAGES_PATH or GIF_IMAGE_PATH does not exist.
+        """
         if os.path.exists(TRAIN_IMAGES_PATH) and os.path.exists(GIF_IMAGE_PATH):
             images = [
                 imageio.imread(os.path.join(TRAIN_IMAGES_PATH, image))
@@ -70,6 +140,13 @@ class Charts:
             raise Exception("Train images path not found.".capitalize())
 
     def plot(self):
+        """
+        Plots comparison images for each set of noisy, clean, and predicted images in the test dataset.
+        Saves a comparison figure to TEST_IMAGES_PATH and a GIF of training images to GIF_IMAGE_PATH.
+
+        Raises:
+            Exception: If the TEST_IMAGES_PATH does not exist.
+        """
         try:
             self.model = DnCNN().to(self.device)
         except Exception as e:
@@ -130,5 +207,26 @@ class Charts:
 
 
 if __name__ == "__main__":
-    charts = Charts()
-    charts.plot()
+    parser = argparse.ArgumentParser(description="Define the Charts".title())
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="mps",
+        help="Device type for PyTorch operations".capitalize(),
+    )
+
+    parser.add_argument(
+        "--test",
+        action="store_true",
+        help="Define the arguments for testing".capitalize(),
+    )
+
+    args = parser.parse_args()
+
+    if args.test:
+
+        charts = Charts(device=args.device)
+        charts.plot()
+
+    else:
+        raise Exception("Define the arguments for testing".title())
