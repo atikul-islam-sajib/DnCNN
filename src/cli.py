6713480518e180f2f8b5ffedcc5f5fe040a062ca
Atikul Islam Sajib <asajib142099@bscse.uiu.ac.bd>
2024-04-06 14:36:29 +0200

    doc string is added in the CLI #17

--- src/cli.py
+++ src/cli.py
@@ -11,6 +11,53 @@ from test import Charts
 
 
 def cli():
+    """
+    Command-Line Interface for the Deep Convolutional Neural Network (DnCNN) for image denoising.
+
+    This CLI supports various operations including unzipping the dataset, preparing data loaders,
+    training the DnCNN model, and evaluating the model's performance through visualization of
+    denoising results on test images.
+
+    Arguments:
+    --image_path : str
+        Path to the zip file containing the dataset. Defaults to './data/data.zip'.
+    --batch_size : int
+        Batch size for training and testing. Defaults to 16.
+    --image_size : int
+        The size to which the images will be resized. Defaults to 64.
+    --split_ratio : float
+        The ratio of the dataset to be used as the test set. Defaults to 0.2.
+    --epochs : int
+        Number of training epochs. Defaults to 100.
+    --lr : float
+        Learning rate for the optimizer. Defaults to 1e-4.
+    --device : str
+        Computing device ('cuda', 'cpu', 'mps'). Defaults to 'mps'.
+    --display : bool
+        Whether to display training progress and metrics. Defaults to True.
+    --beta1 : float
+        Beta1 hyperparameter for Adam optimizer. Defaults to 0.9.
+    --train : store_true
+        Flag to initiate model training.
+    --test : store_true
+        Flag to initiate model testing and visualization.
+    --adam : bool
+        Whether to use Adam optimizer. Defaults to True.
+    --SGD : bool
+        Whether to use Stochastic Gradient Descent (SGD) optimizer. Defaults to False.
+    --is_l1 : bool
+        Whether to use L1 regularization. Defaults to False.
+    --is_l2 : bool
+        Whether to use L2 regularization. Defaults to False.
+    --is_huber_loss : bool
+        Whether to use Huber loss as the training criterion. Defaults to False.
+    --is_weight_clip : bool
+        Whether to apply weight clipping. Defaults to False.
+
+    Example usage:
+    python script_name.py --train --epochs 50 --batch_size 32 --lr 0.0001
+    python script_name.py --test --device cpu
+    """
     parser = argparse.ArgumentParser(description="Define the CLI for DnCNN".title())
 
     parser.add_argument(
